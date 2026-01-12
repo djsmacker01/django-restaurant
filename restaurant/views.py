2c3bbedcc08a698740c7c34f5c7264b9e7e954d3
Nurudeen Adedeji <48442354+djsmacker01@users.noreply.github.com>
2026-01-12 19:22:30 +0000

    Refactor menu_list view function

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from .models import MenuItem
 
 
-# Create your views here.
+
 def menu_list(request):
     """
     View function for displaying the menu list.
     """
      # Get all menu items from database
     menu_items = MenuItem.objects.all()
-
-    # Prepare data to send to template
+e
     context = {
         'menu_items': menu_items
     }
    
-   #render the template with the data
+ 
     return render(request, 'restaurant/menu_list.html', context)
